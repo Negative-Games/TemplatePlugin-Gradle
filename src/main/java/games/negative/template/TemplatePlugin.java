9a93b3ad53a634aca6214a56d26465d6d9ce3d3d
Negative <negggative@gmail.com>
2023-03-04 15:45:16 -0400

    first commit

---
+++ src/main/java/games/negative/template/TemplatePlugin.java
@@ -0,0 +1,18 @@
+package games.negative.template;
+
+import games.negative.framework.BasePlugin;
+
+public final class TemplatePlugin extends BasePlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
